9adcd432f43108b9e00fa3b1ac77b0c48d516639
Darryl.Mendonez <darryl.mendonez@Laptop.home>
2020-06-25 23:03:07 -0400

    Updates

---
+++ precache-manifest.ee5ce1646adbeccfd05dae4643dcf118.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c75e857b71a7d8a2f1acc8b49896059a",
+    "url": "/darrylmendonez/react-crud-todo-list/index.html"
+  },
+  {
+    "revision": "b98c10057b3cb53b3a8f",
+    "url": "/darrylmendonez/react-crud-todo-list/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "3dbd5ee6b110ff0c5553",
+    "url": "/darrylmendonez/react-crud-todo-list/static/js/2.8482d346.chunk.js"
+  },
+  {
+    "revision": "5dedfc9e814c6faf0491be333adc943f",
+    "url": "/darrylmendonez/react-crud-todo-list/static/js/2.8482d346.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b98c10057b3cb53b3a8f",
+    "url": "/darrylmendonez/react-crud-todo-list/static/js/main.d92a5a8d.chunk.js"
+  },
+  {
+    "revision": "36aa99326bb8270e8801",
+    "url": "/darrylmendonez/react-crud-todo-list/static/js/runtime-main.1fe044da.js"
+  }
+]);
